b951bc2ef9807716e27e98611a228822e24dae60
baswarahm <baswarahafizh@gmail.com>
2024-12-27 13:03:10 +0700

    baru

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        primary : '#d946ef' ,
+        dark : '#581c87'
+      }
+    },
+  },
+  plugins: [],
+}
+
